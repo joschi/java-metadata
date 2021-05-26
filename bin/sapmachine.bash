b28fb5d26f99655886538100b19bde3877d862fd
Jochen Schalanda <jochen@schalanda.name>
2021-05-26 23:09:30 +0200

    Fix OS detection for SapMachine

--- bin/sapmachine.bash
+++ bin/sapmachine.bash
@@ -47,10 +47,10 @@ function download {
 		if echo "${asset_name}" | grep -q 'rpm$'
 		then
 			# shellcheck disable=SC2016
-			regex='s/^sapmachine-(jdk|jre)-([0-9].{1,})\.x86_64\.rpm$/IMAGE_TYPE="$1" VERSION="$2" OS="linux" ARCH="x64" EXT="rpm"/g'
+			regex='s/^sapmachine-(jdk|jre)-([0-9].+)\.x86_64\.rpm$/IMAGE_TYPE="$1" VERSION="$2" OS="linux" ARCH="x64" EXT="rpm"/g'
 		else
 			# shellcheck disable=SC2016
-			regex='s/^sapmachine-(jdk|jre)-([0-9].{1,})_(linux|osx|windows)-(x64|aarch64|ppc64|ppc64le)_bin\.(.+)$/IMAGE_TYPE="$1" VERSION="$2" OS="$3" ARCH="$4" EXT="$5"/g'
+			regex='s/^sapmachine-(jdk|jre)-([0-9].+)_(linux|macos|osx|windows)-(x64|aarch64|ppc64|ppc64le)_bin\.(.+)$/IMAGE_TYPE="$1" VERSION="$2" OS="$3" ARCH="$4" EXT="$5"/g'
 		fi
 
 		local IMAGE_TYPE=""
